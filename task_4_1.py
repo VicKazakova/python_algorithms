2f676705627b4ebe327d4b845310b1a61b9c9d48
VicKazakova <78705245+VicKazakova@users.noreply.github.com>
2021-06-29 16:39:53 +0300

    Add files via upload

--- task_4_1.py
+++ task_4_1.py
@@ -1,2 +1,46 @@
 # 1. Проанализировать скорость и сложность одного любого алгоритма, разработанных в рамках домашнего задания первых
 # трех уроков. Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
+
+# 2_4. Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...Количество элементов (n) вводится с
+# клавиатуры.
+import timeit
+
+
+def first_var():
+    n = int(input('Введите количество элементов: '))
+    i = 0
+    number = 1
+    total_sum = 0
+    while i < n:
+        total_sum += number
+        number = number * (-0.5)
+        i += 1
+    print(f'Сумма {n} элементов равна: {total_sum}')
+
+
+def second_var():
+    n = int(input('Введите количество элементов: '))
+    # i = 0
+    # number = 1
+    # total_sum = 0
+
+    def recursion(n_f, i_f, number_f, total_sum_f):
+        if i_f == n_f:
+            return f'Сумма {n} элементов равна: {total_sum_f}'
+        elif i_f < n_f:
+            return recursion(n_f - 1, i_f, number_f * (-0.5), total_sum_f + number_f)
+
+
+# print(recursion(n, i, number, total_sum))
+
+time_track1 = (timeit.timeit('first_var()', setup='from __main__ import first_var', number=1))
+time_track2 = (timeit.timeit('second_var()', setup='from __main__ import second_var', number=1))
+print(f'Без рекурсии: {time_track1}')
+print(f'С рекурсией: {time_track2}')
+if time_track1 < time_track2:
+    print(f'Вариант без рекурсии быстрее в {time_track2 / time_track1} раз')
+else:
+    print(f'Вариант c рекурсией быстрее в {time_track1 / time_track2} раз')
+
+'''При вводе тестовых значений (1000, 5000, 10000 и т.д. элементов) в данном случае всегда получается, 
+что вариант с рекурсией быстрее примерно в 1.5 - 2 раза'''
--- task_4_2.py
+++ task_4_2.py
@@ -1,3 +1,58 @@
 # 2. Написать два алгоритма нахождения i-го по счёту простого числа. Без использования «Решета Эратосфена»; Используя
-# алгоритм «Решето Эратосфена» Примечание ко всему домашнему заданию: Проанализировать скорость и сложность
-# алгоритмов. Результаты анализа сохранить в виде комментариев в файле с кодом.
+# алгоритм «Решето Эратосфена».
+import timeit
+
+
+def first_var():
+    i = int(input('Без решета: Введите число i: '))
+    n = i * 100
+    lst = [2]
+    for m in range(3, n + 1, 2):
+        if (m > 10) and (m % 10 == 5):
+            continue
+        for j in lst:
+            if j * j - 1 > m:
+                lst.append(m)
+                break
+            if m % j == 0:
+                break
+        else:
+            lst.append(m)
+    print(lst[i - 1])
+
+
+def second_var():
+    i = int(input('С решетом: Введите число i: '))
+    n = i * 100
+    a = [0] * n
+    for z in range(n):
+        a[z] = z
+    a[1] = 0
+
+    m = 2
+    while m < n:
+        if a[m] != 0:
+            j = m * 2
+            while j < n:
+                a[j] = 0
+                j = j + m
+        m += 1
+    b = []
+    for z in a:
+        if a[z] != 0:
+            b.append(a[z])
+    del a
+    print(b[i - 1])
+
+
+time_track1 = (timeit.timeit('first_var()', setup='from __main__ import first_var', number=1))
+time_track2 = (timeit.timeit('second_var()', setup='from __main__ import second_var', number=1))
+print(f'Без решета Эратосфена: {time_track1}')
+print(f'С использованием решета Эратосфена: {time_track2}')
+if time_track1 < time_track2:
+    print(f'Вариант без использования решета Эратосфена быстрее в {time_track2 / time_track1} раз')
+else:
+    print(f'Вариант c использованием решета Эратосфена быстрее в {time_track1 / time_track2} раз')
+
+'''При вводе тестовых значений (1000, 5000, 10000 и т.д. элементов) в данном случае всегда получается, 
+что вариант с использованием решета Эратосфена быстрее примерно в 1.0 - 2.1 раза'''
