04a5615961ae5cf079efce19c5480ec1b39aaedc
VicKazakova <78705245+VicKazakova@users.noreply.github.com>
2021-07-03 13:48:00 +0300

    Add files via upload

--- task_5_1.py
+++ task_5_1.py
@@ -2,3 +2,29 @@
 # числа) для каждого предприятия.. Программа должна определить среднюю прибыль (за год для всех предприятий) и
 # вывести наименования предприятий, чья прибыль выше среднего и отдельно вывести наименования предприятий,
 # чья прибыль ниже среднего.
+import collections
+less_average = collections.deque()
+more_average = collections.deque()
+
+number = int(input('Введите кол-во предприятий: '))
+firm_list = []
+for m in range(number):
+    i = input(f'Введите название предприятия {m + 1}: ')
+    firm_list.append(i)
+firm_dict = {firm_list[i]: [float(input(f'Введите данные для предприятия {firm_list[i]}. '
+                                        f'Квартал {q + 1}: '))
+                            for q in range(4)] for i in range(len(firm_list))}
+all_profits = [(sum(profits)) for _, profits in firm_dict.items()]
+avr_profit = sum(all_profits) / number
+firm_profit = list(zip(firm_dict, all_profits))
+for firm in firm_profit:
+    if firm[1] < avr_profit:
+        less_average.append(firm[0])
+    elif firm[1] > avr_profit:
+        more_average.append(firm[0])
+    else:
+        pass
+print('*' * 20)
+print(f'Средняя прибыль для {number} предприятий за год равна: {avr_profit}')
+print(f'Прибыль ниже среднего у следующих фирм: {list(less_average)}')
+print(f'Прибыль выше среднего у следующих фирм: {list(more_average)}')
--- task_5_2.py
+++ task_5_2.py
@@ -2,3 +2,40 @@
 # массив, элементы которого это цифры числа. Например, пользователь ввёл A2 и C4F. Сохранить их как [‘A’,
 # ‘2’] и [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’,
 # ‘E’].
+import collections
+
+
+def from_hex_to_dex(n_hex):
+    n_dex = 0
+    num = collections.deque(n_hex)
+    num.reverse()
+    for i in range(len(num)):
+        n_dex += dex_list[num[i]] * 16 ** i
+    return n_dex
+
+
+def from_dex_to_hex(numb):
+    n_hex = collections.deque()
+    while numb > 0:
+        d = numb % 16
+        for i in dex_list:
+            if dex_list[i] == d:
+                n_hex.append(i)
+        numb //= 16
+    n_hex.reverse()
+    return list(n_hex)
+
+
+hex_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+dex_list = collections.defaultdict(int)
+final = 0
+for i in hex_list:
+    dex_list[i] += final
+    final += 1
+
+
+n1 = from_hex_to_dex(input('Введите первое число в шестнадцатиричном формате:\n ').upper())
+n2 = from_hex_to_dex(input('Введите второе число в шестнадцатиричном формате:\n ').upper())
+
+print(f'Сумма чисел: {from_dex_to_hex(n1 + n2)}')
+print(f'Произведение чисел: {from_dex_to_hex(n1 * n2)}')
